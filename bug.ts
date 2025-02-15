d48d6f66033d9cf71c71b1d259354f9b064ac1ea
BugHunter-X <askzenai@gmail.com>
2025-02-15 01:20:04 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,8 @@
+function printNumbers(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(5); // This works fine
+printNumbers(-5); // This will cause an infinite loop
